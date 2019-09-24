5f2423296df4f73e06dcec66265c411b6702c6d1
H. Chase Stevens <cstevens@teikametrics.com>
2019-09-23 20:18:18 -0400

    Initial commit

---
+++ dockerfile_requirements/__init__.py
@@ -0,0 +1,49 @@
+import os
+import sys
+
+import git
+from jinja2 import Template
+
+
+def git_root():
+    root, = os.popen('git rev-parse --show-toplevel')
+    return root.strip()
+
+
+def get_last_changed(fname):
+    repo = git.Repo(git_root())
+    blame = repo.blame('HEAD', os.path.abspath(fname))
+    line_dt = {line: commit.authored_datetime for commit, block in blame for line in block}
+    def key(reqline):
+        last_modified = line_dt[reqline]
+        return last_modified, reqline
+    return key
+
+
+def read_requirements(fname):
+    with open(fname) as f:
+        req_lines = filter(None, f.read().splitlines())  # todo also need to filter out comments etc. but whatever
+    return sorted(req_lines, key=get_last_changed(fname))
+
+
+ADD_REQUIREMENTS_MACRO = """
+{%- macro add_requirements(fname) -%}
+# Requirements populated from {{fname}}
+{% for requirement in read_requirements(fname) -%}
+RUN pip install {{ requirement }}
+{% endfor -%}
+{%- endmacro -%}
+"""
+
+
+def main(dockerfile_fname=None):
+    if dockerfile_fname is None:
+        dockerfile_fname = sys.argv[1]
+    with open(dockerfile_fname, 'r') as f:
+        template = Template('\n'.join((ADD_REQUIREMENTS_MACRO, f.read())))
+    print(template.render(read_requirements=read_requirements))
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
+
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+setup(
+    name='dockerfile-requirements',
+    packages=['dockerfile_requirements'],
+    platforms='any',
+    version='0.0.1',
+    description='Inlining requirements.txt files into Dockerfiles for better caching.',
+    author='H. Chase Stevens',
+    author_email='chase@chasestevens.com',
+    url='https://github.com/hchasestevens/dockerfile-requirements',
+    license='MIT',
+    install_requires=[
+        'gitpython>=3.0.2',
+        'jinja2>=2.10.1',
+    ],
+    entry_points={
+        'console_scripts': [
+            'dockerfile-requirements = dockerfile_requirements:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+    ]
+)
+
